820bbe5ff5811c7427796431e698fec33b40c55a
Jonas Cesconetto <jonascesconetto@edu.univali.br>
2020-04-14 20:26:39 -0300

    Comentarios exclusivos dos bancos.

--- api/src/infra/database.js
+++ api/src/infra/database.js
@@ -4,7 +4,10 @@ let connection = null;
 module.exports.connect = async () => {
   if (connection === null) {
     connection = await mongoose.createConnection(
+      // Data Base Hosang
       //"mongodb+srv://omnistack:omnistack@omnistack-u2wzm.mongodb.net/restaurantemoderno?retryWrites=true&w=majority",
+
+      // Data Base Jonas -> Base de Teste
       "mongodb+srv://jonas:test01234@cluster0-wmhmj.mongodb.net/restaurante?retryWrites=true&w=majority",
       {
         bufferMaxEntries: 0,
