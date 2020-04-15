40943d9bdecef530e9ef2206939d3c4fdb6e0827
Jonas Cesconetto <jonascesconetto@edu.univali.br>
2020-04-14 21:52:22 -0300

    Update

--- api/src/infra/database.js
+++ api/src/infra/database.js
@@ -8,6 +8,7 @@ module.exports.connect = async () => {
       //"mongodb+srv://omnistack:omnistack@omnistack-u2wzm.mongodb.net/restaurantemoderno?retryWrites=true&w=majority",
 
       // Data Base Jonas -> Base de Teste
+      //
       "mongodb+srv://jonas:test01234@cluster0-wmhmj.mongodb.net/restaurante?retryWrites=true&w=majority",
       {
         bufferMaxEntries: 0,
